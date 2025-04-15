294f0a742063e6eba476451f81b70de8c057632c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-04-14 23:39:12 -0500

    chore: learning quick sort

---
+++ cs-core/11-extra/dsa-python-practice/sorting_algos/quick_sort.py
@@ -0,0 +1,44 @@
+def partition(array:list[int], low:int, high:int):
+
+  # choose the rightmost element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+def quickSort(array: list[int], low: int, high: int):
+  if low < high:
+    pivot = partition(array, low, high)
+
+    quickSort(array, low, pivot - 1)
+
+    quickSort(array, pivot + 1, high)
+
+
+data = [8, 7, 2, 1, 0, 9, 6]
+print("Unsorted Array")
+print(data)
+
+size = len(data)
+
+quickSort(data, 0, size - 1)
+
+print('Sorted Array in Ascending Order:')
+print(data)
