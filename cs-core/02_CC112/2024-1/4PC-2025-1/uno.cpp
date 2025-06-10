688c7984cc5f3f88097077ce7fd9a7374a923c7a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-06-10 15:39:07 -0500

    feat: add methods to classes

--- cs-core/02_CC112/2024-1/4PC-2025-1/uno.cpp
+++ cs-core/02_CC112/2024-1/4PC-2025-1/uno.cpp
@@ -7,7 +7,7 @@ public:
   int numerator;
   int denominator;
 
-  Fraction(int numerator, int denominator = 1) {
+  Fraction(int numerator = 0, int denominator = 1) {
     if (denominator == 0) {
       throw invalid_argument("Denominator cannot be zero");
     }
@@ -17,5 +17,26 @@ public:
 
     this->denominator = abs(denominator);
     this->numerator = abs(numerator) * symbol;
+  }
+
+  Fraction difference(const Fraction &other) const {
+    int commonDenominator = this->denominator * other.denominator;
+    int newNumerator = (this->numerator * other.denominator) -
+                       (other.numerator * this->denominator);
+
+    return Fraction(newNumerator, commonDenominator);
+  }
+
+  Fraction product(const Fraction &other) const {
+    return Fraction(this->denominator * other.denominator,
+                    this->numerator * other.numerator);
+  }
+
+  Fraction division(const Fraction &other) const {
+    if (other.numerator == 0) {
+      throw invalid_argument("Cannot divide by zero");
+    }
+    return Fraction(this->numerator * other.denominator,
+                    this->denominator * other.numerator);
   }
 };
